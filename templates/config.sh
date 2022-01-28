66117dd056adc19b5daaf85d3d33a3bff71e3725
Anton Lugovoi <alugovoi@ordercapital.com>
2022-01-28 15:22:16 +0000

    replace lexicon to cfhookbash

---
+++ templates/config.sh
@@ -0,0 +1,12 @@
+# Instead of api_token, you can also use your global API key. For example:
+#     global_api_key="YOUR_GLOBAL_KEY"
+#     zones="YOUR_ZONES"
+#     email="admin@example.com"
+
+case ${1} in
+	"ocap.cc")
+		api_token="FTYRW2sD18rpv6IFaoyQsYWyf9DI81Aoq9niaOVi"
+		zones="d614fc07212fa5bb1af13d5ada933f3c"
+	;;
+esac
+
